e73a00505c40560faef99fdf661f39f32585f34c
Christopher Beckham <chrispy645@gmail.com>
2014-05-07 12:03:30 +1200

    Cleanup

--- copy-files-to-remote.sh
+++ copy-files-to-remote.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -z $EXPERIMENT_DIR ] || [ -z $REMOTE_NAME ]; then
+	echo "Have you sourced your project's env.sh file?"
+	exit 1
+fi
+
 # Copies experimental directory to remote directory.
 
-scp -r $EXPERIMENT_DIR $REMOTE_NAME
+echo "Copying directory: "$EXPERIMENT_DIR
+echo "To: "$REMOTE_NAME:$EXPERIMENT_DIR
+
+scp -r $EXPERIMENT_DIR/remote $REMOTE_NAME:$EXPERIMENT_DIR
